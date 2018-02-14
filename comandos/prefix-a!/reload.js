104fb3cba0e51d2ffd66367488890ed1195a5aed
DMVMarcio <31147955+DMVMarcio@users.noreply.github.com>
2018-02-14 20:29:45 -0200

    reload

--- comandos/prefix-a!/reload.js
+++ comandos/prefix-a!/reload.js
@@ -7,11 +7,14 @@ const dono = "315263840268976128"
 
     let reason = args.slice(0).join(' ');
 
-    if (message.author.id !== dono) return message.reply("**Você não pode usar este comando!**");
+    if (message.author.id === "315263840268976128" || message.author.id === '399302842688733195'){
     if (reason.length < 1) return message.reply('**Diga o comando que devo reiniciar!**');
 
     delete require.cache[require.resolve(`./${args[0]}.js`)];
 
     message.channel.sendMessage("**:gear: " + message.author + " Comando " + args[0] + " reiniciado!**");
 
+} else {
+    message.reply("**Sem permissão. :confused:**");
+}
 }
