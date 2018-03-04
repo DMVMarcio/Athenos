6f2bc3a418bb48032472d071da4891f2c3017999
DMVMarcio <31147955+DMVMarcio@users.noreply.github.com>
2018-03-04 20:48:49 -0300

    Add files via upload

--- comandos/prefix-a!/shoppppppppp.js
+++ comandos/prefix-a!/shoppppppppp.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
 
 message.channel.sendMessage({
   "embed": {
-    "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ LOJA ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\nBem vindo a loja, aqui você poderá comprar coisas com os seus coins.\nㅤ\n**Como usar:** a!comprar <numero do item>\nㅤ\n**1** - VIP BRONZE (**13721 coins**)\n**2** - VIP PRATA (**18217 coins**)\n**3** - VIP OURO (**23832 coins**)\n**4** - XP x2 (**17981 coins**)\n**5** - COINS x2 (**20712 coins**)\n**6** - PERMISSÃO PARA DIVULGAR (**19321 coins**)\n**7** - WHITE-LIST (**15030 coins**)\n**8** - COMANDO DE LEMBRETE (**5001 coins**)\n**9** - NSFW (**25607**)",
+    "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ LOJA ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\nBem vindo a loja, aqui você poderá comprar coisas com os seus coins.\nㅤ\n**Como usar:** a!comprar <numero do item>\nㅤ\n**1** - VIP BRONZE (**13721 coins**)\n**2** - VIP PRATA (**18217 coins**)\n**3** - VIP OURO (**23832 coins**)\n**4** - XP x2 (**17981 coins**)\n**5** - COINS x2 (**20712 coins**)\n**6** - PERMISSÃO PARA DIVULGAR (**19321 coins**)\n**7** - WHITE-LIST (**15030 coins**)\n**8** - COMANDO DE LEMBRETE (**5001 coins**)\n**9** COINS x4 (**42501**)\n**10** - NSFW (**25607**)",
     "color": 55512,
 
     "thumbnail": {
