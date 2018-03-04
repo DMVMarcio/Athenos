fcf66f3aa6b175031d645424c7837f7641aa3d9f
DMVMarcio <31147955+DMVMarcio@users.noreply.github.com>
2018-03-04 20:07:18 -0300

    Add files via upload

--- comandos/prefix-a!/nsfwemb.js
+++ comandos/prefix-a!/nsfwemb.js
@@ -1,14 +1,13 @@
 exports.run = (client, message, args) => {
 
     message.channel.sendMessage({
-      "embed": {
-        "description": "**[clique aqui](https://discord.gg/SZFyZHw) para tocar uma musica :smile:**",
-        "color": 55512,
-    
-        "thumbnail": {
-          "url": "https://i.imgur.com/4JaNmFp.png"
+        "embed": {
+            "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ NSFW ❄**ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Como usar:**\nㅤ```+rule34 <tema>```\n**Será enviado uma imagem ou gif de acordo com o tema. :relaxed:**",
+            "color": 55512,
+            "thumbnail": {
+                "url": "https://i.imgur.com/4JaNmFp.png"
+            }
         }
-      }
     });
     
     }
