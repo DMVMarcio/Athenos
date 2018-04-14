ec2f2fceaec7f97135af5c9cc436cd7a70342fb9
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-04-14 19:20:55 -0300

    Update emb.js

--- comandos/prefix-a!/emb.js
+++ comandos/prefix-a!/emb.js
@@ -1,14 +1,13 @@
 exports.run = (client, message, args) => {
 
-message.channel.sendMessage({
-  "embed": {
-    "description": "**[clique aqui](https://discord.gg/SZFyZHw) para tocar uma musica :smile:**",
-    "color": 55512,
-
-    "thumbnail": {
-      "url": "https://i.imgur.com/4JaNmFp.png"
-    }
-  }
-});
-
+    message.channel.sendMessage({
+      "embed": {
+        "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ATHENOS ❄**ㅤㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Requisitos:**\nㅤ\nㅤ**-** Mínimo 90 users ( **Poderá haver exceções** )\nㅤ**-** Servidor organizado ( **Canais, cargos etc...** )\nㅤ**-** Tenha uma boa staff.\nㅤ**-** Ter o bot <@421759842382774272> no servidor.\nㅤ**-** Membros que respeitem as regras.\nㅤ\n**Se você tiver todos os requisitos, fale com algum staff ou dono.**",
+        "color": 55512,
+        "thumbnail": {
+          "url": "https://i.imgur.com/4JaNmFp.png"
+        }
+      }
+    });
+    
 }
