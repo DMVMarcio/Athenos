a0b195146416ad39dfd872d2e5eb98b42b0c27af
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-04-14 18:39:45 -0300

    Update anun.js

--- comandos/prefix-a!/anun.js
+++ comandos/prefix-a!/anun.js
@@ -1,10 +1,11 @@
 exports.run = (client, message, args) => {
 
+    if (message.author.id === "315263840268976128" || message.author.id === '399302842688733195' || message.author.id === '286144811680137218'){
     message.delete();
     message.channel.sendMessage("<@&399759520089374720> :arrow_down: :arrow_down: <@&399759520089374720>");
     message.channel.sendMessage({
         "embed": {
-          "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ANUNCIO ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\nSistema de loteria foi criado no servidor, informações em <#410084948930854912>. :wink:",
+          "description": `ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ANUNCIO ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\nSistema de loteria foi criado no servidor, informações em <#410084948930854912>. :wink:`,
           "url": "https://discordapp.com",
           "color": 55512,
           "thumbnail": {
@@ -12,5 +13,8 @@ exports.run = (client, message, args) => {
           }
         }
       });
+    } else {
+     message.reply("**Sem permissão.**);   
+    }
     
     }
