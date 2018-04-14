3417d37cb08595dcf5f9433694a500599fbe5b2b
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-04-14 18:51:29 -0300

    Update anun.js

--- comandos/prefix-a!/anun.js
+++ comandos/prefix-a!/anun.js
@@ -1,20 +1,20 @@
 exports.run = (client, message, args) => {
 
-    if (message.author.id === "315263840268976128" || message.author.id === '399302842688733195' || message.author.id === '286144811680137218'){
-    message.delete();
-    message.channel.sendMessage("<@&399759520089374720> :arrow_down: :arrow_down: <@&399759520089374720>");
-    message.channel.sendMessage({
-        "embed": {
-          "description": `ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ANUNCIO ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\nSistema de loteria foi criado no servidor, informações em <#410084948930854912>. :wink:`,
-          "url": "https://discordapp.com",
-          "color": 55512,
-          "thumbnail": {
-            "url": "https://i.imgur.com/4JaNmFp.png"
-          }
+  if (message.author.id === "315263840268976128" || message.author.id === '399302842688733195' || message.author.id === '286144811680137218'){
+  message.delete();
+  message.channel.sendMessage("<@&399759520089374720> :arrow_down: :arrow_down: <@&399759520089374720>");
+  message.channel.sendMessage({
+      "embed": {
+        "description": `ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ANUNCIO ❄**ㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n${message.content.replace("a!anun ")}`,
+        "url": "https://discordapp.com",
+        "color": 55512,
+        "thumbnail": {
+          "url": "https://i.imgur.com/4JaNmFp.png"
         }
-      });
-    } else {
-     message.reply("**Sem permissão.**);   
-    }
-    
-    }
+      }
+    });
+  } else {
+   message.reply("**Sem permissão.**");   
+  }
+  
+  }
