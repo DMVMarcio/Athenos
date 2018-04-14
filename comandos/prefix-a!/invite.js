1e19111aacf4e72e794ff9b54f9ae3cf57513d8e
Márcio DEV <31147955+DMVMarcio@users.noreply.github.com>
2018-04-14 20:16:49 -0300

    Add files via upload

--- comandos/prefix-a!/invite.js
+++ comandos/prefix-a!/invite.js
@@ -2,7 +2,7 @@ exports.run = (client, message, args) => {
 
     message.channel.sendMessage({
         "embed": {
-          "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ATHENOS ❄**ㅤㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Aqui está o link de convite para você convidar seus amigos para o servidor:**\nhttps://discord.gg/9WUTx4A\nㅤ\nNo momento estamos com **" + message.guild.members.size + " membros** :smile:",
+          "description": "ㅤㅤㅤㅤㅤㅤㅤㅤㅤ**❄ ATHENOS ❄**ㅤㅤㅤㅤㅤㅤㅤㅤㅤ\nㅤ\n**Aqui está o link de convite para você convidar seus amigos para o servidor:**\nhttps://discord.gg/kRX4q\nㅤ\nNo momento estamos com **" + message.guild.members.size + " membros** :smile:",
           "color": 55512,
           "timestamp": "2018-01-23T18:21:28.492Z",
           "footer": {
